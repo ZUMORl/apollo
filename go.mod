3062ef89cebb41f4d9a953f36655a5988cd4fbae
Bohdan-TheOne <BohdanCh.the.One@gmail.com>
2021-07-09 14:06:13 +0300

    Ping test

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/apollo
+
+go 1.16
+
+require (
+	github.com/go-redis/redis v6.15.9+incompatible
+)
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/go-redis/redis"
+)
+
+func main() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+}
