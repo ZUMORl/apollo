9972cac6c964a9ace7b2e7a9eb359a0392360a11
Bohdan-TheOne <BohdanCh.the.One@gmail.com>
2021-07-12 11:43:08 +0300

    Add read, update device

--- entities/device.go
+++ entities/device.go
@@ -2,6 +2,7 @@ package entities
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 
 	"github.com/go-redis/redis"
@@ -23,26 +24,35 @@ func NewDevice(id int, param ...string) *Device {
 	return dev
 }
 
-func (dev Device) Create(cli *redis.Client) {
+func (dev Device) Create(cli *redis.Client) (err error) {
 	json, err := json.Marshal(dev)
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
 
 	err = cli.Set(fmt.Sprintf("devices:%v", dev.id), json, 0).Err()
-	if err != nil {
-		fmt.Println(err)
+	return err
+}
+
+func (dev Device) Update(cli *redis.Client) (err error) {
+	return dev.Create(cli)
+}
+
+func (dev Device) Delete(cli *redis.Client) (err error) {
+	return nil
+}
+
+func (dev *Device) Read(cli *redis.Client) (err error) {
+	val, err := cli.Get(fmt.Sprintf("devices:%v", dev.id)).Result()
+	switch err {
+	case redis.Nil:
+		return errors.New("such device not exist yet")
+	case nil:
+		break
+	default:
+		return err
 	}
-}
 
-func (dev Device) Update(cli *redis.Client) {
-
-}
-
-func (dev Device) Delete(cli *redis.Client) {
-
-}
-
-func (dev Device) Get(cli *redis.Client) {
-
+	err = json.Unmarshal([]byte(val), dev)
+	return err
 }
