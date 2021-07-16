96955bc6c008c064f7afc482689397f03c1ee21d
Bohdan-TheOne <BohdanCh.the.One@gmail.com>
2021-07-16 13:27:53 +0300

    Clear main

--- main.go
+++ main.go
@@ -1,31 +1,9 @@
 package main
 
 import (
-	_ "github.com/apollo/entities"
-
-	"fmt"
-	"os"
-
-	"github.com/go-redis/redis"
+	_ "github.com/apollo/db"
 )
 
-func checkConnection(cli *redis.Client) error {
-	_, err := cli.Ping().Result()
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-	fmt.Println("Connected successfuly")
-	return nil
-}
-
 func main() {
-	var client = redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
-		DB:       0,
-	})
-	if err := checkConnection(client); err != nil {
-		os.Exit(1)
-	}
+
 }
