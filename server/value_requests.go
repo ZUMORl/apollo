59f0c55dbb4db8d4a16faf4ad81e9ab06b39b42f
Bohdan-TheOne <BohdanCh.the.One@gmail.com>
2021-09-29 12:47:03 +0300

    Update value reqests

--- server/value_requests.go
+++ server/value_requests.go
@@ -1,39 +1,30 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strconv"
 
+	"github.com/apollo/db"
 	"github.com/labstack/echo/v4"
 )
 
 // srv.GET("/devices/:d_id/sensors/:s_id/values", getValues)
 func getValues(c echo.Context) error {
-	var req = c.Request()
-	if req.Header["Content-Type"][0] != "application/json" {
-		return c.JSON(http.StatusBadRequest,
-			fmt.Sprintf("%s is not accepted content type",
-				req.Header["Content-Type"][0]))
-	}
-
-	var start, err = strconv.Atoi(req.Header["Start-Value-Index"][0])
-	var end, err_1 = strconv.Atoi(req.Header["End-Value-Index"][0])
+	var start, err = strconv.Atoi(c.QueryParam("start"))
+	var end, err_1 = strconv.Atoi(c.QueryParam("end"))
 	if err != nil || err_1 != nil {
 		return c.JSON(http.StatusBadRequest,
 			fmt.Sprintf("Wrong headers values : %s, %s",
-				req.Header["Start-Value-Index"][0], req.Header["End-Value-Index"][0]))
+				c.QueryParam("start"), c.QueryParam("end")))
 	}
 
 	values, err := sensor.GetValues(c.Param("s_id"), start, end)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError,
-			ServerError{
-				err.Error(),
-				fmt.Sprintf("GET /device/%v/sensors/%v/values",
-					c.Param("d_id"), c.Param("s_id")),
-				"",
-			})
+		return &echo.HTTPError{Code: http.StatusInternalServerError,
+			Message: "Error retrieving sensor values", Internal: err}
 	}
 
 	return c.JSON(http.StatusOK, values)
@@ -41,12 +32,37 @@ func getValues(c echo.Context) error {
 
 // srv.DELETE("/devices/:d_id/sensors/:s_id/values", deleteValues)
 func deleteValues(c echo.Context) error {
-
-	return nil
+	var start, err = strconv.Atoi(c.QueryParam("start"))
+	var end, err_1 = strconv.Atoi(c.QueryParam("end"))
+	if err != nil || err_1 != nil {
+		return c.JSON(http.StatusBadRequest,
+			fmt.Sprintf("Wrong headers values : %s, %s",
+				c.QueryParam("start"), c.QueryParam("end")))
+	}
+	if err := sensor.RemoveValue(c.Param("s_id"), start, end); err != nil {
+		return &echo.HTTPError{Code: http.StatusInternalServerError,
+			Message: "Error deleting sensor values", Internal: err}
+	}
+	return c.NoContent(http.StatusOK)
 }
 
 // srv.POST("/devices/:d_id/sensors/:s_id/values", newValue)
 func newValue(c echo.Context) error {
+	var newValue = db.Value{}
+	bits, err := ioutil.ReadAll(c.Request().Body)
+	if err != nil {
+		return &echo.HTTPError{Code: http.StatusInternalServerError,
+			Message: "Error reading body", Internal: err}
+	}
 
-	return nil
+	if err := json.Unmarshal(bits, &newValue); err != nil {
+		return &echo.HTTPError{Code: http.StatusInternalServerError,
+			Message: "Error parsing body json", Internal: err}
+	}
+
+	if err := sensor.AddValue(c.Param("s_id"), &newValue); err != nil {
+		return &echo.HTTPError{Code: http.StatusInternalServerError,
+			Message: "Error adding value", Internal: err}
+	}
+	return c.NoContent(http.StatusOK)
 }
